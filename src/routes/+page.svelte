2137a40d1bc1a731d7da8fbc43c601f55841d68c
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-06 00:56:24 +0700

    feat: initial project structure from sveltekit cli

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 style="font-size: 3rem; text-align: center; margin-top: 20%;">
+  NEXUS Platform - Status: OPERASIONAL
+</h1>
+```    5.  Sekarang, **simpan file tersebut** dengan menekan `Ctrl + S`.
