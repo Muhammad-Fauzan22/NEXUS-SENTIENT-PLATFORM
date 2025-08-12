f1b7d79381e6b616ef0d318980fdefb155b90250
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-12 21:59:49 +0700

    Auto-sync: auto_git_sync.log, vite.config.js, vite.config.js.timestamp-1755010786682-6b7028b1583a7.mjs - 2025-08-12 21:59:49

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		// Menggunakan lingkungan 'jsdom' untuk mensimulasikan browser,
-		// krusial untuk pengujian komponen Svelte.
 		environment: 'jsdom',
-
-		// Secara eksplisit mendefinisikan file mana yang dianggap sebagai tes.
 		include: ['src/**/*.{test,spec}.{js,ts}'],
-
-		// Memuat file setup sebelum setiap tes dijalankan untuk mengkonfigurasi
-		// lingkungan pengujian, seperti menambahkan matchers jest-dom.
 		setupFiles: ['./vitest-setup.ts']
 	}
 });
