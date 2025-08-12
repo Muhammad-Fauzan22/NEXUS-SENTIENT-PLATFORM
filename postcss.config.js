76d8ba9ba279a0ac92c0941f92ed029cab7037c0
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-12 22:00:16 +0700

    Auto-sync: auto_git_sync.log, postcss.config.js - 2025-08-12 22:00:16

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,13 @@ export default {
     tailwindcss: {},
     autoprefixer: {},
   },
+}```
+
+**PATH:** `tailwind.config.js`
+```javascript
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: { extend: {} },
+  plugins: [],
 }
