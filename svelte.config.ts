eb7a52808f2b76e118f768b9bd79ab10cb4a247c
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-12 13:30:22 +0700

    Auto-sync: auto_git_sync.log, svelte.config.ts - 2025-08-12 13:30:22

--- svelte.config.ts
+++ svelte.config.ts
@@ -13,9 +13,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 
-		// Define path aliases for robust imports
+		// Define path aliases
 		alias: {
-			'$lib': './src/lib'
+			$lib: './src/lib'
 		}
 	}
 };
