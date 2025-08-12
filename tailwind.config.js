d9eb943c068f6ac372e34a4fdba72b6074f3d02d
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-12 22:11:52 +0700

    Auto-sync: auto_git_sync.log, tailwind.config.js - 2025-08-12 22:11:52

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Memberitahu Tailwind untuk memindai semua file sumber yang relevan
+  // untuk mendeteksi kelas utilitas yang digunakan, memastikan file CSS
+  // produksi hanya berisi gaya yang diperlukan.
   content: ['./src/**/*.{html,js,svelte,ts}'],
+
   theme: {
+    // Di sini Anda dapat memperluas palet warna, font, dll.
+    // default dari Tailwind.
     extend: {},
   },
+
+  // Di sini Anda dapat menambahkan plugin Tailwind resmi atau pihak ketiga,
+  // seperti @tailwindcss/typography atau @tailwindcss/forms.
   plugins: [],
-}
+};
