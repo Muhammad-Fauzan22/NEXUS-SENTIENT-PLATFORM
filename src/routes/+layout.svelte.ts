f1e6f2e7d6ab2018594deb289e623a93c5bac274
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-13 13:43:48 +0700

    Auto-sync: auto_git_sync.log, src/routes/+layout.svelte.ts - 2025-08-13 13:43:48

---
+++ src/routes/+layout.svelte.ts
@@ -0,0 +1,8 @@
+// src/routes/+layout.server.ts
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals: { getSession } }) => {
+	return {
+		session: await getSession(),
+	};
+};
