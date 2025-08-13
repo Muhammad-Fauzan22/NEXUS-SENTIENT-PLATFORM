a64dcd75c15e914a1deff8e4bb93c9984e7d15d5
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-13 13:45:21 +0700

    Auto-sync: auto_git_sync.log, src/routes/+layout.ts - 2025-08-13 13:45:21

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,8 +6,8 @@ export const load: PageLoad = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
 	const supabase = createBrowserClient(
-		import.meta.env.VITE_PUBLIC_SUPABASE_URL,
-		import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
+		PUBLIC_SUPABASE_URL,
+		PUBLIC_SUPABASE_ANON_KEY,
 		{
 			global: {
 				fetch,
