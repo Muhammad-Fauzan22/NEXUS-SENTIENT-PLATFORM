4d50fd09b7e11fec7c1955b202ed6768a358b072
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-16 23:29:19 +0700

    Auto-sync: auto_git_sync.log, src/lib/client/supabase.ts - 2025-08-16 23:29:19

--- src/lib/client/supabase.ts
+++ src/lib/client/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '$env/dynamic/public';
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(
+    import.meta.env.VITE_PUBLIC_SUPABASE_URL,
+    import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY
+);
 
