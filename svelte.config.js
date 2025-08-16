6fa5ace12864624c06da85741faf612d23e61662
Muhammad Fauzan <punyofauzan3@gmail.com>
2025-08-17 00:00:45 +0700

    Auto-sync: auto_git_sync.log, svelte.config.js - 2025-08-17 00:00:45

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Menggunakan vitePreprocess memungkinkan Vite untuk menangani semua
-	// pra-pemrosesan (TypeScript, PostCSS, dll.) secara efisien.
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto secara cerdas memilih adapter yang tepat untuk lingkungan
-		// deployment Anda (Vercel, Netlify, dll.), memberikan fleksibilitas maksimum.
-		adapter: adapter(),
+		adapter: adapter({ out: 'build' }),
 
 		// Mendefinisikan alias path '$lib' adalah krusial untuk impor yang bersih
 		// dan konsisten di seluruh aplikasi.
